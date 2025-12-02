ee23ba5f7eba7c1550db33d52fbe5a4efb0d5951
durunduk2020-huhsde <durunduk2020@mail.ru>
2025-12-02 11:24:21 +0500

    Initial commit

---
+++ Assets/script/move.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, 0, 0.1f) * Time.deltaTime*speed);
+    }
+}
